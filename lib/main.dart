48b9a6c3eef3dc586cb520fa3db8da8467646bfe
NAWIF <nnawif@gmail.com>
2019-02-01 22:22:07 +0300

    fixing merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'post.dart';
 import 'groupView.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() {
   runApp(ListViewApp());
@@ -99,13 +100,16 @@ class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    
       appBar: AppBar(
         title: Text(event.name),
       ),
       body: Align(
-          alignment: Alignment.topRight,
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        
+        alignment: Alignment.topRight,
+        child: Column(
+        
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           Text(
             'تاريخ الأنتهاء: ${event.date}',
@@ -123,12 +127,27 @@ class SecondRoute extends StatelessWidget {
               color: Colors.deepOrangeAccent,
             ),
           ),
-          Text(event.date),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain, // otherwise the logo will be tiny
-              child: const FlutterLogo(),
-            ),
+
+          Align(
+            alignment: Alignment.center,
+            child: new CircularPercentIndicator(
+                radius: 180.0,
+                lineWidth: 20.0,
+                animation: true,
+                percent: 0.7,
+                center: new Text(
+                  "70.0%",
+                  style:
+                      new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+                ),
+                footer: new Text(
+                  "منجز من الفعالية",
+                  style:
+                      new TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
+                ),
+                circularStrokeCap: CircularStrokeCap.round,
+                progressColor: Colors.purple,
+              ),
           ),
         ],
       )),
